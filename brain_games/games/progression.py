fa9df3a6d94eabe4aa8ec19e91f4a543b3d0c609
Polina017 <kapkaeva017@yandex.ru>
2022-04-15 23:15:04 +0300

    correct mistakes

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,10 +5,7 @@ GAME_RULES = 'What number is missing in the progression?'
 
 
 def generate_question_answer():
-    number_1 = random.randint(1, 20)
-    number_2 = random.randint(1, 5)
-    number_3 = number_1 + 10 * number_2
-    progression = list(range(number_1, number_3, number_2))
+    progression = generate_progression()
     secret_index = random.randint(0, len(progression) - 1)
     progression = list(map(str, progression))
     symbol = progression[secret_index]
@@ -16,3 +13,11 @@ def generate_question_answer():
     answer = symbol
     question = ("Question: " + " ".join(progression))
     return question, answer
+
+
+def generate_progression():
+    number_1 = random.randint(1, 20)
+    number_2 = random.randint(1, 5)
+    number_3 = number_1 + 10 * number_2
+    progression = list(range(number_1, number_3, number_2))
+    return progression
